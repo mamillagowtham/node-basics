90ee46953daea1ece38cffff664d76a2d5578847
mamillagowtham <mamillagowtham@gmail.com>
2024-12-22 23:29:11 +0530

    external modules

--- index.js
+++ index.js
@@ -1,3 +1,62 @@
 
+// read file async
 
-fs.readFile("./")
+// const fs = require("fs");
+ 
+// fs.readFile("./text.txt","utf-8" ,(err,data)=>{
+// try{
+//   console.log(data);
+// }
+// catch(err)
+// {
+//   console.log(err);
+// }
+// })
+
+  //read file 
+
+// const fs = require("fs");
+ 
+// const data = fs.readFileSync("./text.txt","utf-8");
+// try{
+//     console.log(data)
+// }
+// catch(error){
+//  console.log(error)
+// }
+
+
+//write file async 
+
+// const fs = require("fs");
+ 
+//  fs.writeFileSync("./text.txt"," hi my name is gowtham " , (err,data)=>{
+//     try{
+//         console.log(data)
+//     }
+//     catch(error){
+//      console.log(error)
+//     }
+// });
+
+
+
+// append file
+
+
+// const fs = require("fs");
+
+// fs.appendFile("./text.txt" ,"\nlearn about append file ",(error)=>{
+//     try 
+//     {
+//        console.log("hello")
+//     }
+//     catch
+//     { 
+//        console.log(error)
+//     }
+   
+// })
+
+
+
